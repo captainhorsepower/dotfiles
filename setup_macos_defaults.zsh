3e72bd152bdf859a96c024573908e7c9128982e0
Artsiom Varabei <35666863+captainhorsepower@users.noreply.github.com>
2021-04-07 00:13:32 +0300

    add stuff for vscode vim

--- setup_macos_defaults.zsh
+++ setup_macos_defaults.zsh
@@ -33,6 +33,11 @@ defaults write com.apple.dock tilesize -int 44
 defaults write com.apple.dock largesize -int 73
 defaults write com.apple.dock autohide -bool true
 
+# for vscode Vim; is a restart necessary?
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
+# defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
+# defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false    # For VS Codium
+# defaults delete -g ApplePressAndHoldEnabled                                      # If necessary, reset global default
 # defaults write com.apple.dock autohide-time-modifier -float 0.25
 # defaults write com.apple.dock autohide-delay -float 0.1
 
